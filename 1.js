e91a3bdcd8c74bb5c700b7d0a2eace4639e14873
nadiakohli <andruknadia@gmail.com>
2021-12-20 23:03:05 +0200

    1) description of tasks in English

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
-// 1. З масива objects вивести об'єкт в якому буде мін, макс і середнє арифметичне мін макс
+// 1. From the array of objects to output the object in which there will be min, max and arithmetic mean min max
 
 const objects = [
     {age: 10}, 
@@ -11,8 +11,6 @@ const objects = [
 
 const minNumber = [...objects].sort((min, max) => min.age - max.age)[0].age; 
 const maxNumber = [...objects].sort((min, max) => max.age - min.age)[0].age;
-const minMaxAverageNumbers = (minNumber + maxNumber)/2;
-console.log(minNumber);
-console.log(maxNumber);
-console.log(minMaxAverageNumbers);
+const minMaxAverageNumbers = ( minNumber + maxNumber ) / 2;
+
 
