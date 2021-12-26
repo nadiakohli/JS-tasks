08df3fa4326b6de24db58036416fcd4105a87c26
nadiakohli <andruknadia@gmail.com>
2021-12-26 22:10:38 +0200

    make a task '3'

--- tasks/3/index.js
+++ tasks/3/index.js
@@ -1,8 +1,14 @@
 // 3. From the array numbers to deduce a new array in which to replace
 // positive values on 1, negative values on -1, zero values leave
 
-const getReplacedArray = (array) => {
-  // your code
-};
+const getReplacedArray = (array) => array
+  .map(arr => {
+    if (arr > 0) {
+      return 1;
+    } if (arr < 0) {
+      return -1;
+    }
+    return 0;
+  });
 
 export default getReplacedArray;
