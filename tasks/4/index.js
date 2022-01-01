9f6e81dc23f65be59da4a845eae17d6d2f75af75
nadiakohli <andruknadia@gmail.com>
2022-01-01 21:13:28 +0200

    fix bag

--- tasks/4/index.js
+++ tasks/4/index.js
@@ -1,5 +1,8 @@
 // 4. From the array of numbers to deduce a new array of values of each element multiplying by 2
 
-const getMultiplyingArray = (array) => array.map(number => number * 2);
+const getMultiplyingArray = (array) => {
+  array
+    .map((number) => number * 2);
+};
 
 export default getMultiplyingArray;
