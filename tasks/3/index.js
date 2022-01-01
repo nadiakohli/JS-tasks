40160fa30efd0244de8e3ddae4c1b1640149cfb2
nadiakohli <andruknadia@gmail.com>
2022-01-01 20:56:48 +0200

    chenged some bag

--- tasks/3/index.js
+++ tasks/3/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-else-return */
 // 3. From the array numbers to deduce a new array in which to replace
 // positive values on 1, negative values on -1, zero values leave
 
@@ -6,11 +5,11 @@ const getReplacedArray = (array) => array
   .map((value) => {
     if (value > 0) {
       return 1;
-    } else if (value < 0) {
-      return -1;
-    } else {
-      return 0;
     }
+    if (value < 0) {
+      return -1;
+    }
+    return 0;
   });
 
 export default getReplacedArray;
