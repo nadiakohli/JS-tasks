15c6957f4ad6ddbc1c5ed0f3bb71afcd540c01d7
nadiakohli <andruknadia@gmail.com>
2022-01-01 21:02:43 +0200

    fix some bags

--- tasks/6/index.js
+++ tasks/6/index.js
@@ -1,13 +1,15 @@
 // 6. Output a new array from the Numbers array,
 // divide the positive values by 2, multiply the negative values by -1
 
-const getReplacedArray = (array) => array.map((number) => {
-  if (number > 0) {
-    return number / 2;
-  } if (number < 0) {
-    return number * -1;
-  }
-  return 0;
-});
+const getReplacedArray = (array) => array
+  .map((number) => {
+    if (number > 0) {
+      return number / 2;
+    }
+    if (number < 0) {
+      return number * -1;
+    }
+    return 0;
+  });
 
 export default getReplacedArray;
