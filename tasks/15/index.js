79d00bc268c837b781edee718db999c279f6fe7f
nadiakohli <andruknadia@gmail.com>
2022-01-19 20:45:34 +0200

    task 15

--- tasks/15/index.js
+++ tasks/15/index.js
@@ -1,6 +1,7 @@
 // 15. Output an array of objects whose values are
 // greater than 10 and less than 100 from the array of objects
 
-const getFilteredArray = (array) => {};
+const getFilteredArray = (array) => array
+  .filter((obj) => obj.some > 10 && obj.some < 100);
 
 export default getFilteredArray;
