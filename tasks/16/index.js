edd0b47360129c3c57b4fcd23557e3283ce37046
nadiakohli <andruknadia@gmail.com>
2022-01-20 20:27:50 +0200

    done task 16

--- tasks/16/index.js
+++ tasks/16/index.js
@@ -1,6 +1,7 @@
 // 16. From the array of objects to output an array of objects whose values do not
 // fall under the selected conditions (greater than 10 and less than 100)
 
-const getFilteredArray = (array) => {};
+const getFilteredArray = (array) => array
+  .filter((obj) => !(obj.some > 10 && obj.some < 100));
 
 export default getFilteredArray;
