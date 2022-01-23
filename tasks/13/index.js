932bc7f6eedcd92f39a31d00be96beb21b15dccf
nadiakohli <andruknadia@gmail.com>
2022-01-23 11:27:25 +0200

    fixed issue

--- tasks/13/index.js
+++ tasks/13/index.js
@@ -1,6 +1,6 @@
 // 13. Output numbers greater than 10 and less than 100 from the array of numbers
 
 const getFilteredArray = (array) => array
-  .filter(item => (typeof item === 'number') && item < 100 && item > 10);
+  .filter((item) => item < 100 && item > 10);
 
 export default getFilteredArray;
