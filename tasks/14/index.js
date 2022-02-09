63b37f121194564f58fc70908231e26c10501520
nadiakohli <andruknadia@gmail.com>
2022-02-09 21:54:42 +0200

    done task 14

--- tasks/14/index.js
+++ tasks/14/index.js
@@ -1,6 +1,6 @@
 // 14. Output numbers from the array of numbers that do not fall under the
 // selected conditions (greater than 10 and less than 100)
 
-const getFilteredArray = (array) => {};
+const getFilteredArray = (array) => array.filter((item) => !(item > 10 && item < 100));
 
 export default getFilteredArray;
