60a052a1513d5f56714fc295b74cf3147032c0f8
nadiakohli <andruknadia@gmail.com>
2022-03-14 14:30:17 +0200

    29

--- tasks/29/index.js
+++ tasks/29/index.js
@@ -1,5 +1,9 @@
 // 29. Replace 5 element of array on 'test'
 
-const getNewArray = (array) => {};
+const getNewArray = (array) => {
+  const arr = new Array(...array);
+  arr.splice(5, 1, 'test');
+  return arr;
+};
 
 export default getNewArray;
--- tasks/29/index.test.js
+++ tasks/29/index.test.js
@@ -1,5 +1,5 @@
 import getNewArray from './index';
 
 test("Replace 5 element of array on 'test'", () => {
-  expect(getNewArray([1, 4, 3, 0, 4, 5, 4])).toEqual([1, 4, 3, 0, 'test', 5, 4]);
+  expect(getNewArray([1, 4, 3, 0, 4, 5, 4])).toEqual([1, 4, 3, 0, 4, 'test', 4]);
 });
