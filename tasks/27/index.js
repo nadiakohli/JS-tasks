406a52c8e103b543076a43a56629c4858b4a6c45
nadiakohli <andruknadia@gmail.com>
2022-03-14 14:36:45 +0200

    27

--- tasks/27/index.js
+++ tasks/27/index.js
@@ -1,5 +1,9 @@
 // 27. Delete 2 elements from array which after second element
 
-const getNewArray = (array) => {};
+const getNewArray = (array) => {
+  const spliceArr = new Array(...array);
+  spliceArr.splice(2, 2);
+  return spliceArr;
+};
 
 export default getNewArray;
