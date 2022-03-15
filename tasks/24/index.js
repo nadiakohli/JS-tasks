06fe71334d7f50a80d887a412f63c15d64723b40
nadiakohli <andruknadia@gmail.com>
2022-03-15 20:23:10 +0200

    24

--- tasks/24/index.js
+++ tasks/24/index.js
@@ -1,5 +1,11 @@
 // 24. Return the string with the replaced values. Replace all vowels letters on 'b' letter
 
-const getString = (string) => {};
+const getString = (string) => {
+  const vowels = ['a', 'o', 'i', 'e', 'u'];
+  const letters = string.split('');
+  const indexes = letters
+    .reduce((acc, vowel, index) => vowels.includes(vowel) ? [...acc, index] : acc, []);
+  return letters.map((letter, index) => indexes.includes(index) ? 'b' : letter).join('');
+};
 
 export default getString;
