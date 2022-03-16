55cd1698a301de91ba2f90f7c64660c6fd7e4684
nadiakohli <andruknadia@gmail.com>
2022-03-16 18:45:39 +0200

    23

--- tasks/23/index.js
+++ tasks/23/index.js
@@ -1,5 +1,13 @@
 // 23. Write function which will cut a string after 8 symbol and add 3 dots at the end of the row
 
-const getString = (string) => {};
+const getString = (string) => {
+  const letters = string.split('');
+  letters.reduce((acc, letter) => {
+    if (letter === ' ') return acc;
+    const modifiedLetters = letters.splice(8, 3, '...');
+    return modifiedLetters;
+  }, []);
+  return letters.join('');
+};
 
 export default getString;
