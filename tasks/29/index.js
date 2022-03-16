9ad1e98a8ed3ee9d3947edc302455b53c08b2f7e
nadiakohli <andruknadia@gmail.com>
2022-03-16 22:38:44 +0200

    29

--- tasks/29/index.js
+++ tasks/29/index.js
@@ -1,9 +1,8 @@
 // 29. Replace 5 element of array on 'test'
 
 const getNewArray = (array) => {
-  const arr = new Array(...array);
-  arr.splice(5, 1, 'test');
-  return arr;
+  array.splice(5, 1, 'test');
+  return array;
 };
 
 export default getNewArray;
