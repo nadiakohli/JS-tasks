2f04ce660b23535b4c4495e40633b2dc1f30b9d8
nadiakohli <andruknadia@gmail.com>
2022-03-25 16:05:41 +0200

    26

--- tasks/26/index.js
+++ tasks/26/index.js
@@ -1,9 +1,9 @@
 // 26. Delete third element from array. Return new array without deleted element
 
 const getNewArray = (array) => {
-  const newArr = new Array(...array);
-  newArr.splice(2, 1);
-  return newArr;
+  const spliceElement = [...array];
+  spliceElement.splice(2, 1);
+  return spliceElement;
 };
 
 export default getNewArray;
